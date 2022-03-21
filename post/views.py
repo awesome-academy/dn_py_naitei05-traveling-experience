27455e3cc747860156d76ba1c4efd34247eeb319
luuhieu8720 <lethiluuhieu@gmail.com>
2022-03-21 17:00:36 +0700

    Add create-post page

--- post/views.py
+++ post/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-from post.models import Profile
+from post.forms import PostForm
+from post.models import Post, Profile
 
 def index(request):
     messages.error(request, _("Your username and password didn't match. Please try again."))
@@ -28,3 +30,26 @@ def user_management(request):
     }
 
     return render(request, 'admin/user_management.html', context)
+
+@login_required
+def post_create(request):
+    user = Profile.objects.filter(username=request.user).first()
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            print(form.cleaned_data)
+            post = Post()
+            post.image = form.cleaned_data["image"]
+            post.user = user
+            post.content = form.cleaned_data["content"]
+            post.title = form.cleaned_data["title"]
+            post.save()
+        else:
+            messages.error(request, _("ERROR: New post creation failed"))
+    else:
+        form = PostForm(initial={
+                    "user": user,
+                })
+
+    return render(request, 'post/create_post.html', context={"form": form})
